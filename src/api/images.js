d73a44faedf190dae4f72737fc21f069baef7548
Duknam Yoo <yoo2001818@gmail.com>
2017-10-26 22:53:39 +0900

    Add API prototypes

--- src/api/images.js
+++ src/api/images.js
@@ -1,5 +1,46 @@
 import Router from 'express-promise-router';
+import { NotImplementedError } from './util/errors';
 
 const router = new Router();
+
+router.post('/scan', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.get('/', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.get('/:id', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.patch('/:id', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.get('/:id/tags', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.put('/:id/tags', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.delete('/:id/tags', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.get('/:id/tags/:tagId', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.patch('/:id/tags/:tagId', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.delete('/:id/tags/:tagId', (req, res) => {
+  throw new NotImplementedError();
+});
 
 export default router;
--- src/api/tags.js
+++ src/api/tags.js
@@ -1,5 +1,26 @@
 import Router from 'express-promise-router';
+import { NotImplementedError } from './util/errors';
 
 const router = new Router();
+
+router.get('/', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.post('/', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.get('/:id', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.patch('/:id', (req, res) => {
+  throw new NotImplementedError();
+});
+
+router.delete('/:id', (req, res) => {
+  throw new NotImplementedError();
+});
 
 export default router;
