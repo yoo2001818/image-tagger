6eb4d7cf9d6892f817830bfa246739b188041e25
Duknam Yoo <yoo2001818@gmail.com>
2017-11-14 23:58:55 +0900

    Add image entity reducer

--- src/reducer/entities/image.js
+++ src/reducer/entities/image.js
@@ -1,5 +1,49 @@
 import mergeEntities from '../../util/mergeEntities';
+import { FETCH, PATCH, SET, ADD_TAG, REMOVE_TAG, SET_TAG, DESTROY, POST }
+  from '../../action/image';
 
 export default function imageEntityReducer(state = {}, action) {
-  return mergeEntities(state, action, 'image');
+  // First, merge entities.
+  let newState = mergeEntities(state, action, 'image');
+  // Then, run image reducer.
+  if (action.meta.id == null) return newState;
+  let result = imageReducer(newState[action.meta.id], action);
+  if (result === newState[action.meta.id]) return newState;
+  if (result === null) {
+    let output = Object.assign({}, newState);
+    delete output[action.meta.id];
+    return output;
+  }
+  return Object.assign({}, newState, {
+    [action.meta.id]: result,
+  });
+}
+
+export function imageReducer(state = {}, action) {
+  switch (action.type) {
+    case FETCH:
+    case PATCH:
+      if (action.meta.pending || action.error) {
+        return Object.assign({}, state, {
+          pending: true,
+        });
+      }
+      return Object.assign({}, state, {
+        pending: false,
+      });
+    case DESTROY:
+      if (action.meta.pending || action.error) {
+        return Object.assign({}, state, {
+          pending: true,
+        });
+      }
+      return null;
+    case POST:
+    case SET:
+    case ADD_TAG:
+    case REMOVE_TAG:
+    case SET_TAG:
+    default:
+      return state;
+  }
 }
--- src/util/mergeEntities.js
+++ src/util/mergeEntities.js
@@ -3,6 +3,7 @@ export default function mergeEntities(state = {}, action, name) {
   if (action.payload.entities == null) return state;
   if (action.payload.entities[name] == null) return state;
   let entities = action.payload.entities[name];
+  if (Object.keys(entities).length === 0) return state;
   let output = Object.assign({}, state);
   for (let key in entities) {
     output[key] = Object.assign({}, state[key], entities[key]);
