8bad558c4935eeb14ec44a81b478fb758e7dcd89
Duknam Yoo <yoo2001818@gmail.com>
2017-11-22 23:55:48 +0900

    Add SVG bounding box lines

--- src/container/imageItem.js
+++ src/container/imageItem.js
@@ -7,7 +7,24 @@ class ImageItem extends PureComponent {
     const { image } = this.props;
     return (
       <div className='image-item'>
-        <img src={`/api/images/${image.id}/thumb`} width={320} height={180} />
+        <div className='viewport'>
+          <img src={`/api/images/${image.id}/thumb`} width={320} height={180} />
+          <svg className='svg-overlay' width={320} height={180}
+            viewBox='0 0 1 1' preserveAspectRatio='none'
+          >
+            <g className='bounding-box'>
+              <rect x={0.25} y={0.25}
+                width={0.3} height={0.3}
+                className='shadow' />
+              <rect x={0.25} y={0.25}
+                width={0.3} height={0.3}
+                className='bg' />
+              <rect x={0.25} y={0.25}
+                width={0.3} height={0.3}
+                className='fg' />
+            </g>
+          </svg>
+        </div>
         <div className='path'>{ image.path }</div>
       </div>
     );
